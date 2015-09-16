ee3486006dca11d8419bd002132def11bd42e942
vioxx <shomenuchi@gmail.com>
2015-09-15 20:21:37 -0500

    Changes UI

--- server.R
+++ server.R
@@ -1,14 +1,20 @@
 library(shiny)
 library(mcr)
+
 shinyServer(function(input, output) {
+  
   datasetInput <- reactive({
+    
     inFile <- input$file1
     if (is.null(inFile))
       return(NULL)
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-                  quote=input$quote)
-  })
+    read.csv(inFile$datapath, header=input$header, 
+             sep=input$sep, quote=input$quote)
+  
+    })
+  
   output$plot1 <- renderPlot({
+    
     a <- datasetInput()
     if (is.null(a)) {
       return(NULL)} else {
@@ -16,25 +22,34 @@ shinyServer(function(input, output) {
         data1<- mcreg(a$M1,a$M2,
                       mref.name=input$xlab,
                       mtest.name=input$ylab)
-        MCResult.plotDifference(data1, plot.type=input$batype)
+        MCResult.plotDifference(data1, plot.type=input$batype,
+                                ref.line.col=input$poicol)
+      
       }
+    
   })
+  
   output$plot2 <- renderPlot({
+    
     a <- datasetInput()
     if (is.null(a)) {
       return(NULL)} else {
         names(a) <- c("M1", "M2")
         input$regmodel
-        data1<- mcreg(a$M1,a$M2, error.ratio=input$syx, method.reg=input$regmodel, method.ci=input$cimethod)
+        data1<- mcreg(a$M1,a$M2, error.ratio=input$syx, 
+                      method.reg=input$regmodel, method.ci=input$cimethod)
         plot(data1, ci.area=input$ciarea, 
-             add.legend=input$legend, 
-             identity=input$identity,
-             add.cor=input$addcor,
-             x.lab=input$xlab,
-             y.lab=input$ylab)
+             add.legend=input$legend, identity=input$identity,
+             add.cor=input$addcor, x.lab=input$xlab,
+             y.lab=input$ylab, points.pch=input$poipch, points.col=input$poicol
+             )
+        
       }
+    
   })
+  
   output$summary <- renderPrint({
+    
     a <- datasetInput()
     if (is.null(a)) {
       return(NULL)} else {
@@ -42,10 +57,15 @@ shinyServer(function(input, output) {
         data1<- mcreg(a$M1,a$M2)
         printSummary(data1)
       }
-  })
+  
+    })
+  
   output$table <- renderTable({
-  datasetInput()
+    
+    datasetInput()
+    
   })
+  
   output$downloadReport <- downloadHandler(
     filename = function() {
       paste('my-report', sep = '.', switch(
@@ -53,6 +73,7 @@ shinyServer(function(input, output) {
       ))
     },
     content = function(file) {
+      
       src <- normalizePath('report.Rmd')
       owd <- setwd(tempdir())
       on.exit(setwd(owd))
@@ -63,6 +84,9 @@ shinyServer(function(input, output) {
         PDF = pdf_document(), HTML = html_document(), Word = word_document()
       ))
       file.rename(out, file)
+    
     }
+    
   )
+  
 })
